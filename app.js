083cd23f21719566fab9e64558fd32fa9e84e012
Ali <alifeyza4017@gmail.com>
2022-09-19 13:27:34 +0300

    added

--- app.js
+++ app.js
@@ -84,6 +84,25 @@ function setBgGreet() {
     }
 }
 
+// Get Name
+function getName() {
+    if(localStorage.getItem("name") === null) {
+        name.textContent = '[Enter Name]'
+    }else {
+        name.textContent = localStorage.getItem("name")
+    }
+}
+// Get Focus
+function getFocus() {
+    if(localStorage.getItem("focus") === null) {
+        focus.textContent = `[Enter Focus]`
+    }else {
+        focus.textContent = localStorage.getItem("focus")
+    }
+}
+
 // Run
 showTime()
 setBgGreet()
+getName()
+getFocus()
